b70c8be8910ae3bc944b5eb20a8a2bc2073ca91b
Rita <ritajones.gdn@gmail.com>
2026-05-04 12:30:58 +0300

    Add project files.

---
+++ UniGuide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
